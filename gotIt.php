1dfa517d460d4f6bacd0eb7345f015a6d9a6948a
Josh Kogan <jokogan@umich.edu>
2015-04-01 09:43:18 -0400

    chaged gotit

--- gotIt.php
+++ gotIt.php
@@ -3,7 +3,7 @@
     $isItExisting = (file_exists($filename));
 
  	$handle = fopen($filename, 'a');
-	$msg = "Thank you ". $_POST['name'] . " for submitting your information.\n";	//EMail message
+	$msg = "Thank you ". $_POST['firstname'] . " for submitting your information.\n";	//EMail message
 	$stringToAdd="";										//File information
 
 	if (!$isItExisting){
@@ -27,7 +27,7 @@
 	//now close the file
 	fclose($handle); 
 	$to = $_POST["email"];
-	$headers = "From: ". $_POST["name"] ."<".$_POST["email"]. ">\r\n";
+	$headers = "From: ". $_POST["firstname"] ."<".$_POST["email"]. ">\r\n";
 
 	mail($to, 'Form Data', $msg,$headers);
 
