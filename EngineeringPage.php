859965cbe6c0103a35d26aad4e943419b920616a
Josh Kogan <jokogan@umich.edu>
2015-04-13 01:47:20 -0400

    Updated Engineering Page

--- EngineeringPage.php
+++ EngineeringPage.php
@@ -25,11 +25,22 @@
 			<div id="EECS280" class ="course">
 				<p class="description">Techniques and algorithm development and effective programming, top-down analysis, structured programming, testing and program correctness. Program language syntax and static and runtime semantics. Scope, procedure instantiation, recursion, abstract data types and parameter passing methods. Structured data types, pointers, linked data structures, stacks, queues, arrays, records and trees.<br>
 				<button class = "comButton" onclick="showText('280_COM')">Comments...</button>
-					<div class="comments" id="280_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="280_COM">
+						<tr>
+							<td>In 280 I thought the the projects were difficult and took a lot of time, but overall I got good grades on them. I did very poorly on the tests though and as a result got a low grade in the class.</td>
+							<td class="userInfo">EECS Major, 2014 | 10/13/13</td>
+						</tr>
+						<tr>
+							<td>I think 280 is easier if you take/have taken EECS 183 before rather than Engr101</td>
+							<td class="userInfo">Math Major, 2015 | 4/3/13</td>
+						</tr>
+						<tr>
+							<td>The difficulty of 280 is really overstated. I was a very inexperienced programmer going into the class and still managed a B pretty easily. If you put a decent amount of time into it and start the projects early, there's really nothing holding you back from getting at least a B.</td>
+							<td class = "userInfo">EECS Major, 2017 | 3/1/15</td>
+						</tr>
+					</table>
 				</p>
-				<p class="data"></p>
+				<p class="data">Overall Rating: 3.9/5 | Course Difficulty: 4/5 | Average Grade: B</p>
 			</div>
 		</section>
 	<h3>EECS 281: Data Structures and Algorithms</h3>
@@ -37,11 +48,22 @@
 			<div id="EECS281" class ="course">
 				<p class="description">Introduction to algorithm analysis and O-notation; Fundamental data structures including lists, stacks, queues, priority queues, hash tables, binary trees, search trees, balanced trees and graphs; searching and sorting algorithms; recursive algorithms; basic graph algorithms; introduction to greedy algorithms and divide and conquer strategy. Several programming assignments.<br>
 				<button class = "comButton" onclick="showText('281_COM')">Comments...</button>
-					<div class="comments" id="281_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="281_COM">
+						<tr>
+							<td>That class was 100 times worse than 280 and was my hardest/favorite class that I've taken at Michigan</td>
+							<td class="userInfo">EECS Major, 2015 | 9/22/13</td>
+						</tr>
+						<tr>
+							<td>In 281 I couldn't do the projects well. I didn't use the autograder like I should have, but generally the feedback was pretty useless except the grade itself. I also went in thinking it would be like 203 and 280, but it has almost nothing to do with 203</td>
+							<td class="userInfo">EECS Major, 2016 | 1/9/14</td>
+						</tr>
+						<tr>
+							<td>281 is much less about programming and much more about understanding some fundamental theoretical ideas. The programming is just about applying these ideas, and it's not that bad nowadays. The projects were pretty easy to do well on. I felt that the tests were way more difficult.</td>
+							<td class = "userInfo">EECS Major, 2016 | 4/14/14</td>
+						</tr>
+					</table>
 				</p>
-				<p class="data"></p>
+				<p class="data">Overall Rating: 3.5/5 | Course Difficulty: 4.7/5 | Average Grade: B-</p>
 			</div>
 		</section>
 	<h3>EECS 285: A Programming Language or Computer System</h3>
@@ -49,11 +71,22 @@
 			<div id="EECS285" class ="course">
 				<p class="description">A course covering a complex computer system or programming language. Programming problems will be assigned. Specific languages or systems to be offered will be announced in advance.<br>
 				<button class = "comButton" onclick="showText('285_COM')">Comments...</button>
-					<div class="comments" id="285_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="285_COM">
+						<tr>
+							<td>I did not care for EECS 285. I would recommend against taking it.</td>
+							<td class="userInfo">Math Major, 2016 | 5/13/14</td>
+						</tr>
+						<tr>
+							<td>285 was easily one of my favorite classes.</td>
+							<td class="userInfo">EECS Major, 2016 | 12/21/14</td>
+						</tr>
+						<tr>
+							<td>Prof Morgan provides good lecture slides, but the project specs are broad, yet his expectations are very specific. You need a programming background- the projects are time-consuming</td>
+							<td class = "userInfo">EECS Major, 2017 | 3/6/15</td>
+						</tr>
+					</table>
 				</p>
-				<p class="data"></p>
+				<p class="data">Overall Rating: 4.2/5 | Course Difficulty: 3.5/5 | Average Grade: A</p>
 			</div>
 		</section>
 	<h3>EECS 485</h3>
@@ -61,9 +94,16 @@
 			<div id="EECS485" class ="course">
 				<p class="description">Concepts surrounding Web information systems, including client/server systems, security, XML, information retrieval and search engines and data replication issues. Includes substantial final project involving development of a database-backed web site.<br>
 				<button class = "comButton" onclick="showText('485_COM')">Comments...</button>
-					<div class="comments" id="485_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="485_COM">
+						<tr>
+							<td>485 teaches you how to make websites. You'll get practice in Javascript, HTML, CSS, a server architecture of your choice (My group did Flask, another did Node.js, you can pick pretty much anything you want), and you'll also get to try a little bit of Hadoop near the end of the semester</td>
+							<td class="userInfo">EECS Major, 2015 | 10/13/14</td>
+						</tr>
+						<tr>
+							<td>part of what made 485 so good for me was that Mike Cafarella taught it, and he was phenomenal. If you can wait until he's teaching it again, I recommend it.</td>
+							<td class="userInfo">EECS Major, 2016 | 11/18/14</td>
+						</tr>
+					</table>
 				</p>
 				<p class="data"></p>
 			</div>
@@ -73,9 +113,16 @@
 			<div id="EECS493" class ="course">
 				<p class="description">Concepts and techniques for designing computer system user interfaces to be easy to learn and use, with an introduction to their implementation. Task analysis, design of functionality, display and interaction design, and usability evaluation. Interface programming using an object-oriented application framework. Fluency in a standard object-oriented programming language is assumed.<br>
 				<button class = "comButton" onclick="showText('493_COM')">Comments...</button>
-					<div class="comments" id="493_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="493_COM">
+						<tr>
+							<td>I was really looking forward to this course, and it was fine for the first half. I didn't learn a single thing the second half of the course. In fact, the review sheet for the final was the same as the review sheet for the midterm plus two or three additional things.</td>
+							<td class="userInfo">EECS Major, 2014 | 10/13/13</td>
+						</tr>
+						<tr>
+							<td>love this course, very straight forward.  Team project at the end can be a little overwhelming though.</td>
+							<td class="userInfo">EECS Major, 2016 | 2/3/15</td>
+						</tr>
+					</table>
 				</p>
 				<p class="data"></p>
 			</div>
--- LSAPage.php
+++ LSAPage.php
@@ -26,7 +26,7 @@
 				<button class = "comButton" onclick="showText('PSYCH_COM')">Comments...</button>
 					<table class="comments" id="PSYCH_COM">
 						<tr>
-							<td>THIS CLASS SUCKS SO MUCH. I did not like it one bit. Let me tell you</td>
+							<td>THIS CLASS SUCKS SO MUCH. I did not like it one bit. Let me tell you.</td>
 							<td class="userInfo">Psych Major, 2015 | 12/13/14</td>
 						</tr>
 						<tr>
@@ -47,11 +47,22 @@
 			<div id="STATS" class ="course">
 				<p class="description">In this course students are introduced to the concepts and applications of statistical methods and data analysis. Examples of applications are drawn from virtually all academic areas and some attention is given to statistical process control methods.<br>
 				<button class = "comButton" onclick="showText('STATS_COM')">Comments...</button>
-					<div class="comments" id="STATS_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="STATS_COM">
+						<tr>
+							<td>I found this class to be super easy. I took some stats in high school and it was a breeze. Never need to go to class and all the lectures are online..</td>
+							<td class="userInfo">Ross Business School, 2015 | 12/6/13</td>
+						</tr>
+						<tr>
+							<td>Great intro course for stats. I’m currently taking my third stats course at Michigan and this class is by far the easiest, but is a great introduction. Go to class. You will learn better that way.</td>
+							<td class="userInfo">STATS Major, 2016 | 1/24/14</td>
+						</tr>
+						<tr>
+							<td>If you don’t have Gunderson, don’t go to class! Everything is online.</td>
+							<td class = "userInfo">Psych Major, 2017 | 3/24/15</td>
+						</tr>
+					</table>
 				</p>
-				<p class="data"></p>
+				<p class="data">Overall Rating: 4.1/5 | Course Difficulty: 3.4/5 | Average Grade: B+</p>
 			</div>
 		</section>
 	<h3>Math 115: Calculus I</h3>
@@ -59,11 +70,22 @@
 			<div id="Calc" class ="course">
 				<p class="description">The course presents the concepts of calculus from three points of view: geometric (graphs); numerical (tables); and algebraic (formulas). Students will develop their reading, writing and questioning skills. Topics include functions and graphs, derivatives and their applications to real-life problems in various fields, and definite integrals. The classroom atmosphere is interactive and cooperative and homework is done in groups.<br>
 				<button class = "comButton" onclick="showText('CALC_COM')">Comments...</button>
-					<div class="comments" id="CALC_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="CALC_COM">
+						<tr>
+							<td>If you took calc in high school and placed out, do not take this class! It is much harder in college.</td>
+							<td class="userInfo">Econ, 2015 | 3/23/12</td>
+						</tr>
+						<tr>
+							<td>The material is generally easy to grasp, but the exams are much harder than they need to be. Also really didn’t like the GSI’s.</td>
+							<td class="userInfo">Informatics, 2016 | 1/27/13</td>
+						</tr>
+						<tr>
+							<td>Math is my thing and this class was really easy for me. Had the textbook, but barely used it. Group assignments were annoying, but really not that bad.</td>
+							<td class = "userInfo">Psych Major, 2017 | 10/15/14</td>
+						</tr>
+					</table>
 				</p>
-				<p class="data"></p>
+				<p class="data">Overall Rating: 3.2 | Course Difficulty: 4.1/5 | Average Grade: B</p>
 			</div>
 		</section>
 	<h3>Spanish 277: Reading, Grammar, and Composition</h3>
@@ -71,11 +93,22 @@
 			<div id="Spanish" class ="course">
 				<p class="description">This course will meet three hours a week, with one additional hour of individual study on-line. The course will focus on reading short stories in Spanish and learning to write analytic essays. However, the grammatical content will be taught in an on-line format, with points of grammar linked to materials taught and discussed in class.<br>
 				<button class = "comButton" onclick="showText('SPAN_COM')">Comments...</button>
-					<div class="comments" id="SPAN_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="SPAN_COM">
+						<tr>
+							<td>I placed out of Spanish coming to Michigan and thought I would just continue taking the language. Huge mistake! This class was super difficult. I had to speak spanish the whole class and didn’t understand what was going on the whole time. Grading isn’t too bad, but I struggled.</td>
+							<td class="userInfo">ORG Studies, 2014 | 9/3/12</td>
+						</tr>
+						<tr>
+							<td>Loved this class! Super easy and the material we read was actually interesting. My professor was great and the class discussion was always enjoyable!</td>
+							<td class="userInfo">Spanish Major, 2014 | 2/5/13</td>
+						</tr>
+						<tr>
+							<td>Not a terribly difficult class, but certainly not easy. I found myself reading spanish passages over and over again. If you are really good at Spanish then this class should be fine for you.</td>
+							<td class = "userInfo">Psych Major, 2017 | 11/21/14</td>
+						</tr>
+					</table>
 				</p>
-				<p class="data"></p>
+				<p class="data">Overall rating: 3.3 | Course Difficulty: 4.3/5 | Average Grade: B</p>
 			</div>
 		</section>
 	<h3>Screen Arts and Culture (SAC) 236: The Art of Film</h3>
@@ -83,11 +116,22 @@
 			<div id="Film" class ="course">
 				<p class="description">This course examines the dramatic and psychological effects of the elements and techniques used in film making and television, and some of the salient developments in film's artistic and technological history. This course provides students with the basic tools and methods for film appreciation and study.<br>
 				<button class = "comButton" onclick="showText('SAC_COM')">Comments...</button>
-					<div class="comments" id="SAC_COM">
-						<p>THIS CLASS SUCKS SO MUCH</p>
-					</div>
+					<table class="comments" id="SAC_COM">
+						<tr>
+							<td>Great class. Super easy. A bunch of short writing assignments. And tons of watching movies!!</td>
+							<td class="userInfo">Ross Business School, 2016 | 10/11/13</td>
+						</tr>
+						<tr>
+							<td>Thought this class would be easy and interesting, but I hated the director that we focused on, and my class was curved to a B! Had no idea until I got my first assignment back.</td>
+							<td class="userInfo">Sociology Major, 2016 | 1/4/14</td>
+						</tr>
+						<tr>
+							<td>Great intro to film class. Teaches you a lot about the fundamentals of movies and makes you look at all films differently forever. Grading really depends on your GSI though.</td>
+							<td class = "userInfo">Psych Major, 2016 | 4/7/14</td>
+						</tr>
+					</table>
 				</p>
-				<p class="data"></p>
+				<p class="data">Overall Rating: 4.2 | Course Difficulty: 3.4 | Average Grade B+</p>
 			</div>
 		</section>
 	</div>
