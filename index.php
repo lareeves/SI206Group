39a1e28a502553f136588a6979d17bfe9639faec
lareeves <lareeves@umich.edu>
2015-04-14 14:06:51 -0400

    header is set with working footer everywhere

--- index.php
+++ index.php
@@ -11,16 +11,17 @@
 	<h4>
 		Welcome to RateMyClass.com!
 	</h4>
-	<p> 
-		Here at Rate My Class we give you a summary of the classes,</br> 
-		a grade if they pass <em>our</em>  test and some comments from people<br/> 
-		who have taken it. We aim to be the main hub of course information from a student's point of view.
+	<p id="indexPage"> 
+		Here at Rate My Class we give you a summary of classes, a grade to see if they pass <em>our</em> test and some comments
+		</br> from people who have taken the courses. We aim to be the main hub of course information from a student's point of view.</br></br>
+
 		Rate My Class is built for college students, by college students.
-		Click on a college up top to browse </br>
-		through popular courses. <br/>
-		Do you have opinions about a class?<br/>  
-		Share your own stories on the submit page!
+		</br></br>
+		Click on a college to browse through popular courses. Do you have opinions about a class?  Share your own stories on the submit page!
 	</p>
 	</div>
+	<?php 
+		include("footer.php");
+	?>
 </body>
 </html>
