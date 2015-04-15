306ecdeb2ed97e13da36055428c7eb928e49117b
lareeves <lareeves@umich.edu>
2015-04-14 21:25:23 -0400

    fixed scrolling issue, also have columns added

--- head.php
+++ head.php
@@ -6,9 +6,9 @@
 	<link rel="stylesheet" href="http://ginchai.people.si.umich.edu/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/4.2.1/bootstrap-social.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<link href='http://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Average' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="header_style.css">
 	<link rel="stylesheet" href="footer_style.css">
 	<link rel="stylesheet" href="core_page_style.css">
