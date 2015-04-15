c80492af7327ce716bab882e32b921007b2e4b1a
Gin Chairuangsang <Gin@0587376846.wireless.umich.net>
2015-04-15 00:27:34 -0400

    update footer.php

--- footer.php
+++ footer.php
@@ -10,10 +10,10 @@
 	<p id="names">&copy;  Gin Chairuangsang, Josh Kogan, Vikram Rajagopalan, Lauren Reeves </p>
 	<div id="social">
 		<!-- Modified from: http://lipis.github.io/bootstrap-social/ -->
-		<a class="btn btn-social-icon btn-facebook" target="_blank" href="https://www.facebook.com/universityofmichigan">
+		<a alt="link to facebook" class="btn btn-social-icon btn-facebook" target="_blank" href="https://www.facebook.com/universityofmichigan">
 		    <i class="fa fa-facebook"></i>
 		</a>
-		<a class="btn btn-social-icon btn-twitter" target="_blank" href="https://twitter.com/umich">
+		<a alt="link to twitter" class="btn btn-social-icon btn-twitter" target="_blank" href="https://twitter.com/umich">
 			<i class="fa fa-twitter"></i>
 		</a>
 	</div>
